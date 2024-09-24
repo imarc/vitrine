ad7ac440136f15b3db5222e42383066833a34cbf
Kevin Hamer <kevin@imarc.com>
2024-09-24 13:52:11 -0400

    sort component names

--- src/templates/RecursiveList.js
+++ src/templates/RecursiveList.js
@@ -4,9 +4,14 @@ export default {
     data: { type: Array, required: true },
     tag: { type: String, default: 'ul' },
   },
+  computed: {
+    sorted() {
+      return this.data.toSorted((a, b) => a.name > b.name ? 1 : -1)
+    },
+  },
   template: `
     <component :is="tag">
-      <li v-for="child of data">
+      <li v-for="child of sorted">
         <a v-if="'url' in child" :href="child.url">{{ child.name }}</a>
         <RecursiveList v-if="child.children?.length" :data="child.children" :tag="tag" />
       </li>
